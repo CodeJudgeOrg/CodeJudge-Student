f79a85a3c7c4a140316fe59f7075115e4df40f79
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2025-12-27 12:32:19 +0100

    Added Pseudocode for the  backennd using C

---
+++ native/judger.c
@@ -0,0 +1,16 @@
+#include "judger.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* run_judge(const char* code, const char* language, const char* challenge) {
+    // TODO Receive the Code of the user, the language he used and the expected results as a String
+    // TODO Write the users code into a file with the correct ending of the language he used
+    // TODO Call the compiler and pass the file with the users code
+    // TODO Filter the compilers output
+    //          - Syntax errors: Return message "Syntax errors. Did you select the correct language?"
+    //          - Wrong results: Return message "Wrong results"
+    //          - Correct solution: Return message "Succes!"
+    // TODO Delete the file with the users Code
+    // TODO Quit
+}
