023244e75dc2a447b7b9d323aec77e3a71b07d69
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-01-01 14:44:45 +0100

    Generating a file with the users code should now work

--- native/judger.c
+++ native/judger.c
@@ -3,9 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* run_judge(const char* code, const char* language, const char* challenge) {
-    // TODO Receive the Code of the user, the language he used and the expected results as a String
-    // TODO Write the users code into a file with the correct ending of the language he used
+// If called this function receives 3 String and it returns one as soon as it's done
+char* run_judge(const char* usersCode, const char* programmingLanguage, const char* solution) {
+    char* fileName;
+    // Write the users code into a file with the correct ending of the language he used
+    fileName = writeFile(usersCode, ".py");
     // TODO Call the compiler and pass the file with the users code
     // TODO Filter the compilers output
     //          - Syntax errors: Return message "Syntax errors. Did you select the correct language?"
@@ -13,4 +15,18 @@ char* run_judge(const char* code, const char* language, const char* challenge) {
     //          - Correct solution: Return message "Succes!"
     // TODO Delete the file with the users Code
     // TODO Quit
+}
+
+char* writeFile(char* content, char* ending){
+    // Generate the correct file name depending on the langugae the user used
+    size_t len = 8 + strlen(ending);
+    char* fileName = malloc(len);
+    strcpy(fileName, "UserCode");
+    strcat(fileName, ending);
+
+    // Generate a new file or override an existing one
+    FILE *userFile = fopen("", "w");
+
+    // Return the correct file name
+    return fileName;
 }
