d5d270ce6ade945f8b77f9e894da4fddb85bf734
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-01-03 23:28:37 +0100

    Modifications to be able to load the library depending on the plattform

--- lib/utils/judger_loader.dart
+++ lib/utils/judger_loader.dart
@@ -3,12 +3,27 @@ import 'dart:io';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 
-// Load my library into a temporary directory
+// Load my library into a temporary directory (platform dependent)
 Future<DynamicLibrary> loadJudgerLibrary() async {
-  final byteData = await rootBundle.load('assets/native/linux/libjudger.so');
+  late final String assetPath;
+  late final String fileName;
 
+  if (Platform.isLinux) {
+    assetPath = 'assets/native/linux/libjudger.so';
+    fileName = 'libjudger.so';
+  } else if (Platform.isWindows) {
+    assetPath = 'assets/native/windows/judger.dll';
+    fileName = 'judger.dll';
+  } else {
+    throw UnsupportedError("Unsupported platform");
+  }
+
+  // Load the asset
+  final byteData = await rootBundle.load(assetPath);
+
+  // Copy to a temporary directory because DynamicLibrary.open() needs a real file
   final tempDir = await getTemporaryDirectory();
-  final libPath = '${tempDir.path}/libjudger.so';
+  final libPath = '${tempDir.path}/$fileName';
 
   final file = File(libPath);
   await file.writeAsBytes(byteData.buffer.asUint8List());
