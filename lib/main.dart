fd37863ae1e2cafb53c9dd8387d0cb323a5dc2bf
naibaf-1 <175958349+naibaf-1@users.noreply.github.com>
2026-04-23 11:43:30 +0200

    Register the new provider

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,9 @@ import 'package:code_judge/layouts/mobile_layout.dart';
 import 'package:code_judge/layouts/tablet_layout.dart';
 import 'package:code_judge/utils/judger_bindings.dart';
 import 'package:code_judge/utils/judger_loader.dart';
+import 'package:code_judge/utils/my_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:provider/provider.dart';
 
 
@@ -32,12 +32,17 @@ void main() async {
   final lib = await loadJudgerLibrary();
   judgerLib = JudgerLib(lib);
 
+  final submittedExercisesController = SubmittedExercises();
   final settingsController = SettingsController();
+
   //Apply SharedPreferences
   await settingsController.loadSettings();
   runApp(
-    ChangeNotifierProvider(
-      create: (_) => settingsController,
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => submittedExercisesController),
+        ChangeNotifierProvider(create: (_) => settingsController),
+      ],
       child: const MyApp(),
     ),
   );
@@ -128,62 +133,3 @@ class Responsive{
   }
 }
 
-// Apply the settings to the whole app
-class SettingsController extends ChangeNotifier {
-  ThemeMode _selectedTheme = ThemeMode.system;
-  ThemeMode get selectedTheme => _selectedTheme;
-  Locale? _selectedLocale;
-  Locale? get selectedLocale => _selectedLocale;
-  
-  // Get the applied settings from the preferences
-  Future<void> loadSettings() async {
-    final sharedPreferences = await SharedPreferences.getInstance();
-    final localeCode = sharedPreferences.getString('selected_locale');
-    final themeCode = sharedPreferences.getString('selected_theme');
-
-    // Set correct localisation
-    if (localeCode != null) {
-      _selectedLocale = Locale(localeCode);
-    } else {
-      _selectedLocale = null; // System language
-    }
-    // Set correct theme
-    if (themeCode == "light") {
-      _selectedTheme = ThemeMode.light;
-    } else if (themeCode == "dark") {
-      _selectedTheme = ThemeMode.dark;
-    } else {
-      _selectedTheme = ThemeMode.system;
-    }
-    notifyListeners();
-  }
-
-  // Apply correct theme
-  Future<void> applyTheme(ThemeMode mode) async {
-    final sharedPreferences = await SharedPreferences.getInstance();
-
-    if (mode == ThemeMode.system) {
-      await sharedPreferences.remove('selected_theme');
-    } else if (mode == ThemeMode.light){
-      await sharedPreferences.setString('selected_theme', 'light');
-    } else {
-      await sharedPreferences.setString('selected_theme', 'dark');
-    }
-    _selectedTheme = mode;
-    notifyListeners();
-  }
-
-  // Apply the correct language
-  Future<void> applyLanguage(Locale? locale) async {
-    final sharedPreferences = await SharedPreferences.getInstance();
-
-    if (locale == null) {
-      await sharedPreferences.remove('selected_locale');
-      _selectedLocale = null;
-    } else {
-      await sharedPreferences.setString('selected_locale', locale.languageCode);
-      _selectedLocale = locale;
-    }
-    notifyListeners();
-  }
-}
